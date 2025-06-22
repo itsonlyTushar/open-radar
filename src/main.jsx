c1534e237773b4f9bfb5184a4612035c664b2f03
Tushar Soni <tushargsoni@outlook.com>
2025-06-22 17:39:42 +0530

    added vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import { createRoot } from "react-dom/client";
 import AppRoutes from "./routes/AppRoutes.jsx";
 import "./index.css";
 import { HelmetProvider } from "react-helmet-async";
+import { Analytics } from "@vercel/analytics/react"
 
 createRoot(document.getElementById("root")).render(
   <HelmetProvider>
     <AppRoutes />
+    <Analytics />
   </HelmetProvider>
 );
